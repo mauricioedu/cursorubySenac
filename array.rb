6b748803561c38500e58f91dbbd431a7dccb7e3d
Mauricio <olive.y.ra@hotmail.com>
2015-05-30 14:15:23 -0300

    Versão 1.0

---
+++ array.rb
@@ -0,0 +1,13 @@
+#array
+nome = ["Ruby","Java-Android","PHP"]
+
+puts nome
+
+nome << "Pascal"
+
+puts nome 
+
+#Hash
+pessoa = {:nome=>"Mauricio",:email=>"mauricioeduardo@hotmail.com"}
+puts pessoa[:nome]
+puts pessoa[:email]
